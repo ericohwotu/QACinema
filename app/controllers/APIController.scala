cf3ae4b214627ca51763d2b9684ccecfed61d277
James Mountain <james.w.mountain@gmail.com>
2017-08-02 16:29:09 +0000

    Added some basic API calls for movie by ID and movies.

---
+++ app/controllers/APIController.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import models.Movie
+import play.api.mvc._
+
+class APIController extends Controller {
+  def movieByID(id: Int) : Action[AnyContent] = Action {
+    Movie.movies.find(movie => id == movie.id) match {
+      case Some(movie) => Ok(views.html.moviepage(movie))
+      case None => Ok("No movie found!")
+    }
+
+  }
+
+  def movies() : Action[AnyContent] = Action {
+    Ok(views.html.movies(Movie.movies.toList))
+  }
+}
---
+++ app/models/Movie.scala
@@ -0,0 +1,12 @@
+package models
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by Administrator on 01/08/2017.
+  */
+case class Movie(id:Int) {}
+
+object Movie {
+  val movies = ListBuffer[Movie](Movie(1), Movie(2))
+}
