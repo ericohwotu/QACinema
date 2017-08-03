234f4a109f8632c88ca8844f65e24e34ee93457f
James Mountain <james.w.mountain@gmail.com>
2017-08-03 15:03:49 +0000

    Linked up the listings and the movie individual data page
    Changed the payment redirect.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,6 +10,7 @@ import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMo
 import play.api.libs.json._
 import reactivemongo.play.json._
 import reactivemongo.api.Cursor
+import reactivemongo.bson.BSONObjectID
 import reactivemongo.play.json.collection.JSONCollection
 import reactivemongo.play.json.collection.JSONCollection
 
@@ -28,13 +29,30 @@ class Application @Inject()(val reactiveMongoApi : ReactiveMongoApi) extends Con
     futureCursor.flatMap(_.collect[List]())
   }
 
-  def listings: Action[AnyContent] = Action.async { implicit request =>
-    futureMovies.map {
-      movies => Ok(views.html.listings(movies.grouped(3).toList))
-    }
+  def futureIDs: Future[List[JsObject]] = {
+    val futureIDCurs = collection.map {_.find(Json.obj(), Json.obj("_id" -> 1)).cursor[JsObject]()}
+
+    futureIDCurs.flatMap(_.collect[List]())
   }
 
-  def movie(id : String) = Action {
-    Ok(views.html.movie())
+  def listings: Action[AnyContent] = Action.async { implicit request =>
+    futureIDs.flatMap(jsos => {
+      futureMovies.map {
+        movies => {
+          Ok(views.html.listings(movies.zip(jsos.map {
+            jso => ((jso \ "_id").as[JsObject] \ "$oid").as[String]
+          }).grouped(3).toList))
+        }
+      }
+    })
+  }
+
+  def movie(id : String): Action[AnyContent] = Action.async { implicit request =>
+    val futureIDCursor: Future[Cursor[Movie]] = collection.map {_.find(Json.obj("_id" -> BSONObjectID(id))).cursor[Movie]()}
+    val futureIDList : Future[List[Movie]] = futureIDCursor.flatMap(_.collect[List]())
+
+    futureIDList.map {
+      movieIDs => movieIDs.headOption.fold(BadRequest("no movies"))(res => Ok(views.html.movie(res)))
+    }
   }
 }
