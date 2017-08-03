aae20b374dd09879c9acef2eaa5cd5d9d1636815
lewishayqa <lewis.hay@qa.com>
2017-08-03 15:32:05 +0000

    Added the base Contact and Find Us pages. Made some changes to style.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,12 +5,20 @@ import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = Action {
+  def index() = Action {
     Ok(views.html.index())
   }
 
-  def certifications = Action {
+  def certifications() = Action {
     Ok(views.html.certifications())
+  }
+
+  def contactUs() = Action {
+    Ok(views.html.contactUs())
+  }
+
+  def findUs() = Action {
+    Ok(views.html.findUs())
   }
 
 }
