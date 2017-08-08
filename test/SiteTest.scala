4fd4f1a88645a1b4fd8adb11817262e58ca8621c
Ladon <ladonjackson@hotmail.co.uk>
2017-08-08 14:34:43 +0100

    adding link to about page, and first test to navigate to the booking page for a specific movie

--- test/SiteTest.scala
+++ test/SiteTest.scala
@@ -1,15 +1,19 @@
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.scalatest.selenium.HtmlUnit
 import org.scalatest.{FlatSpec, ShouldMatchers}
 
 class SiteTest extends FlatSpec with ShouldMatchers with HtmlUnit {
 
   val host = "http://localhost:9000"
-
-  "Going to home page" should "have a title of homepage" in {
+  "Starting from the main page you " should "be able to go navigate through the site to the booking page for a specific movie" in {
     go.to(host)
-    pageTitle should be ("Homepage")
+    click on linkText("Listings")
+    pageTitle should be ("Listings")
+    click on xpath("/html/body/div/div[2]/div[1]/div[1]/a/img")
+    pageTitle should be("Movie")
+    click on xpath("/html/body/div/div[1]/div/div/a/b/button")
+    pageTitle should not be("Booking")
   }
-
 
 
 }
