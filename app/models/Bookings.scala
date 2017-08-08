ccd0dc4394c85ff2aaaf31e3fcd878dfab9d7f64
ericohwotu <eric.ohwotu@outlook.com>
2017-08-08 15:53:00 +0000

    fix merge conflicts

--- app/models/Bookings.scala
+++ app/models/Bookings.scala
@@ -3,8 +3,12 @@ package models
 import play.api.libs.json.{Json, OFormat}
 
 case class Booking(
-                     seats: List[Seat],
-                     price: Double
-                   )
+                    reference: String,
+                    movieName: String,
+                    date: String,
+                    time: String,
+                    seats: List[Seat],
+                    price: Double
+                  )
 
 object Booking{implicit val bookings :OFormat[Booking] = Json.format[Booking]}
