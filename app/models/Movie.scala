411ddfd5a8178508ea94daac41b09a1c419f2ce5
ericohwotu <eric.ohwotu@outlook.com>
2017-08-08 15:53:00 +0000

    fix merge conflicts

--- app/models/Movie.scala
+++ app/models/Movie.scala
@@ -38,27 +38,3 @@ object Movie{
     )(Movie.apply)(Movie.unapply)
   )
 }
-case class trendingMovieList(title: String)
-
-case class Booking(
-                    reference: String,
-                    movieName: String,
-                    date: String,
-                    time: String,
-                   seats: List[Seat],
-                   price: Double
-                   )
-
-case class CinemaLocation(
-                         latitude: String,
-                         longitude: String
-                         )
-
-object CinemaLocation{ implicit val locations :OFormat[CinemaLocation] = Json.format[CinemaLocation]}
-object Booking{implicit val bookings :OFormat[Booking] = Json.format[Booking]}
-object trendingMovieList{implicit val movieList :OFormat[trendingMovieList] = Json.format[trendingMovieList]}
-
-object JsonFormats {
-  import play.api.libs.json.Json
-  implicit val movieFormat = Json.format[trendingMovieList]
-}
