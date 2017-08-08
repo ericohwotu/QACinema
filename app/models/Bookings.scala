a2223b5f3a4f331972d81f6c8a9ec1164ee1ab52
Ufuoma-Daniel <Ufuoma-Daniel@hotmail.co.uk>
2017-08-08 16:34:16 +0100

    Final

--- app/models/Bookings.scala
+++ app/models/Bookings.scala
@@ -2,9 +2,9 @@ package models
 
 import play.api.libs.json.{Json, OFormat}
 
-case class Bookings(
+case class Booking(
                      seats: List[Seat],
                      price: Double
                    )
 
-object Bookings{implicit val bookings :OFormat[Bookings] = Json.format[Bookings]}
+object Booking{implicit val bookings :OFormat[Booking] = Json.format[Booking]}
