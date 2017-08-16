76bc84ffcb32d486c586fbd6898acc3804b6bc3b
James <james.w.mountain@gmail.com>
2017-08-16 13:49:45 +0000

    Added functionality in the backend to use points

--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -11,29 +11,41 @@ class PaymentController extends Controller {
   val braintreeGateway : BraintreeGateway = new BraintreeGateway("access_token$sandbox$2ywzyb5rtfnk6m6h$10bfae954e82add885cac2188735ccda")
   val noAmount : Result = BadRequest("No amount provided.")
 
+  def calculateDiscountedPrice(price : BigDecimal, pointsUsedString : String) : (BigDecimal, Long) = {
+    val adjustedPrice = price - BigDecimal(pointsUsedString) / 1000
+    if (0 > adjustedPrice) (BigDecimal(0), price.toLong * 1000) else (adjustedPrice, pointsUsedString.toLong)
+  }
+
   def generateClientToken(): String = braintreeGateway.clientToken().generate(new ClientTokenRequest())
   def initiateClientToken() : Action[AnyContent] = Action { implicit request =>
     request.session.get("bookingPrice").fold(noAmount)(am => Ok(views.html.payment(generateClientToken(), am)))
   }
 
-  def makeTransactionRequest(nonce : String, request : Request[AnyContent]) : Result =
-    request.session.get("bookingPrice").fold(noAmount)(am => finalizeRequest(nonce, BigDecimal(am.toString)))
+  def makeTransactionRequest(nonce : String, pointsUsed : String, request : Request[AnyContent]) : Result =
+    request.session.get("bookingPrice").fold(noAmount) { am =>
+      val discountUsage = calculateDiscountedPrice(BigDecimal(am.toString), pointsUsed)
+      finalizeRequest(nonce, discountUsage._1, discountUsage._2, request)
+    }
 
-  def finalizeRequest(nonce: String, amount: BigDecimal) : Result = {
+  def finalizeRequest(nonce: String, amount: BigDecimal, pointsUsed : Long, request : Request[AnyContent]) : Result = {
     val transactionRequest : TransactionRequest = new TransactionRequest()
     transactionRequest.amount(amount.bigDecimal).merchantAccountId("GBP").paymentMethodNonce(nonce)
 
     val result: braintreegateway.Result[Transaction] = braintreeGateway.transaction().sale(transactionRequest)
     if (result.isSuccess) {
-      Ok(s"Transaction Success! Booking made!")
+      Ok(s"Transaction Success! Booking made!").withSession(request.session + ("pointsUsed" -> pointsUsed.toString))
     } else {
       Ok(s"Transaction failure:  ${result.getMessage}")
     }
   }
 
   def makePayment : Action[AnyContent] = Action { implicit request =>
-    val nonce : Seq[String] = request.body.asFormUrlEncoded.getOrElse(Map()).getOrElse("nonce", List[String]())
+    val body: Map[String, Seq[String]] = request.body.asFormUrlEncoded.getOrElse(Map())
+    val nonce : Seq[String] = body.getOrElse("nonce", List[String]())
+    val pointsUsed : Seq[String] = body.getOrElse("pointsUsed", List[String]("0"))
 
-    nonce.headOption.fold(BadRequest("No nonce provided!"))(no => makeTransactionRequest(no, request))
+    nonce.headOption.fold(BadRequest("No nonce provided!"))(no =>
+      makeTransactionRequest(no, pointsUsed.headOption.fold("0")(pu => pu), request)
+    )
   }
 }
